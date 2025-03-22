3efa22bb494a029a25efa74d0ffa977f19d4fb87
raimundo-osf <roliva19@alumnos.utalca.cl>
2025-03-22 13:08:51 -0300

    Production-ready version completed

---
+++ README.md
@@ -0,0 +1,16 @@
+<h1 align="center">✨ Glooba, la Red Social de la sustentabilidad ✨</h1>
+
+### Configuración de archivo .env 
+
+```js
+NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY=
+CLERK_SECRET_KEY=
+DATABASE_URL=
+UPLOADTHING_TOKEN=
+```
+
+### Correr la app
+
+```shell
+npm run dev
+```
---
+++ src/components/DesktopNavbar.tsx
@@ -0,0 +1,50 @@
+import { BellIcon, HomeIcon, UserIcon } from "lucide-react";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+import { SignInButton, UserButton } from "@clerk/nextjs";
+import ModeToggle from "./ModeToggle";
+import { currentUser } from "@clerk/nextjs/server";
+
+async function DesktopNavbar() {
+  const user = await currentUser();
+
+  return (
+    <div className="hidden md:flex items-center space-x-4">
+      <ModeToggle />
+
+      <Button variant="ghost" className="flex items-center gap-2" asChild>
+        <Link href="/">
+          <HomeIcon className="w-4 h-4" />
+          <span className="hidden lg:inline">Inicio</span>
+        </Link>
+      </Button>
+
+      {user ? (
+        <>
+          <Button variant="ghost" className="flex items-center gap-2" asChild>
+            <Link href="/notifications">
+              <BellIcon className="w-4 h-4" />
+              <span className="hidden lg:inline">Notificaciones</span>
+            </Link>
+          </Button>
+          <Button variant="ghost" className="flex items-center gap-2" asChild>
+            <Link
+              href={`/profile/${
+                user.username ?? user.emailAddresses[0].emailAddress.split("@")[0]
+              }`}
+            >
+              <UserIcon className="w-4 h-4" />
+              <span className="hidden lg:inline">Perfil</span>
+            </Link>
+          </Button>
+          <UserButton />
+        </>
+      ) : (
+        <SignInButton mode="modal">
+          <Button variant="default">Iniciar sesión</Button>
+        </SignInButton>
+      )}
+    </div>
+  );
+}
+export default DesktopNavbar;
